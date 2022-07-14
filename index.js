5713c3fa9b943c1963328a17a040d4b1cd538f6a
harumi <firmansyah720.fs@gmail.com>
2022-07-14 22:59:34 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+// Get routes to the variabel
+const router = require('./src/routes')
+// import here
+
+const app = express();
+
+let corsOptions = {
+    origin: '*',
+}
+
+
+const port = process.env.PORT || 5000;
+app.use(express.json());
+app.use(cors(corsOptions));
+
+app.use('/uploads', express.static('uploads'));
+// Add endpoint grouping and router
+app.use('/api/v1/', router);
+
+app.listen(port, () => console.log(`Listening on port ${port}!`));
