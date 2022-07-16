d433f149b4b74e7587bf50b49cf11146103be008
harumi <firmansyah720.fs@gmail.com>
2022-07-17 01:02:51 +0700

    add root server online

--- index.js
+++ index.js
@@ -19,4 +19,11 @@ app.use('/uploads', express.static('uploads'));
 // Add endpoint grouping and router
 app.use('/api/v1/', router);
 
+app.use('/', (req, res) => {
+    res.send({
+        status: "online",
+        message: "Server Is Online!"
+    })
+})
+
 app.listen(port, () => console.log(`Listening on port ${port}!`));
